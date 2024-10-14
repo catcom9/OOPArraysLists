61920a6a5921d633a11b9fc1a049afb0b88eb206
CATHALCOMMINS-STUDEN <G00427615@atu.ie>
2024-10-14 09:33:36 +0100

    Added a try catch block to try and put an element in index 10 of the array

--- src/main/java/ie/atu/week5/Main.java
+++ src/main/java/ie/atu/week5/Main.java
@@ -1,5 +1,7 @@
 package ie.atu.week5;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int[] MyArray = new int[10];
@@ -7,5 +9,18 @@ public class Main {
             MyArray[i] = ((i + 1) * 100);
             System.out.println("Element at index " + i + ": " + MyArray[i]);
         }
+
+        Scanner input = new Scanner(System.in);
+
+        try{
+            System.out.println("Enter a number: ");
+            MyArray[10] = input.nextInt();
+        } catch (Exception e){
+            System.out.println("Im afraid i cant let you do that.");
+        }
+
+
+
+
     }
 }
