35fe6f07d2f9097b6ed8093f37f0179b87e85831
CATHALCOMMINS-STUDEN <G00427615@atu.ie>
2024-10-14 09:26:08 +0100

    Added basic code for working with arrays

--- src/main/java/ie/atu/week5/Main.java
+++ src/main/java/ie/atu/week5/Main.java
@@ -2,7 +2,10 @@ package ie.atu.week5;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        int[] MyArray = new int[10];
+        for (int i = 0; i != 10; i++){
+            MyArray[i] = ((i + 1) * 100);
+            System.out.println("Element at index " + i + ": " + MyArray[i]);
+        }
     }
 }
